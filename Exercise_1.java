c8931111f590bab17259bd6bb6cd4b704da8c431
Omar Alkhuwaytim <84483586+Omaraak@users.noreply.github.com>
2024-07-01 19:40:07 +0300

    Add files via upload

---
+++ Exercise_1.java
@@ -0,0 +1,131 @@
+import java.util.Scanner;
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // EX1
+//        double weight;
+//        double height;
+//        double bmi;
+//
+//        System.out.println("Enter your weight in kg: ");
+//        weight = scanner.nextDouble();
+//
+//        System.out.println("Enter your height in m: ");
+//        height = scanner.nextDouble();
+//
+//        bmi = weight / (height * height);
+//
+//        System.out.println("Your BMI is: " + bmi);
+
+         //EX2
+//        double user_mark;
+//        double total_marks;
+//        double present;
+//
+//        System.out.println("Enter your mark: ");
+//        user_mark = scanner.nextInt();
+//
+//        System.out.println("Enter the total marks");
+//        total_marks = scanner.nextInt();
+//
+//        present = (( user_mark / total_marks) * 100);
+//
+//        System.out.println("You got a " + present + " %");
+
+        //EX3
+//        System.out.println("Welcome to the exchange hub");
+//        double original_currency;
+//        double exchange_rate;
+//        double new_currency;
+//
+//        System.out.println("Could you please provide us withe the amount: ");
+//        original_currency = scanner.nextDouble();
+//
+//        System.out.println("Now could you provide us withe the exchange rate: ");
+//        exchange_rate = scanner.nextDouble();
+//
+//        new_currency = original_currency * exchange_rate;
+//
+//        System.out.println("Thanks for waiting. \nYou have: " + new_currency);
+
+        //EX4
+//       String user_string;
+//
+//       System.out.println("Enter a string: ");
+//       user_string = scanner.nextLine();
+//
+//       StringBuilder sb = new StringBuilder(user_string);
+//
+//       System.out.println("Length of the string is: " + user_string.length() + " And the reversed string is: " + sb.reverse());
+
+
+        //EX5
+//        String text;
+//        int start_index;
+//        int end_index;
+//
+//        System.out.println("Enter the text: ");
+//        text = scanner.nextLine();
+//
+//        System.out.println("Start index: ");
+//        start_index = scanner.nextInt();
+//
+//        System.out.println("End index: ");
+//        end_index = scanner.nextInt();
+//
+//        System.out.println(text.substring(start_index, end_index));
+
+        //EX6
+//        String text;
+//        String word;
+//
+//        System.out.println("Enter the text here: ");
+//        text = scanner.nextLine();
+//
+//        System.out.println("Enter the word here: ");
+//        word = scanner.nextLine();
+//
+//        if (text.contains(word)){
+//            System.out.println("Keyword: " + word + " is present in the text.");
+//        }else{
+//            System.out.println("Keyword: " + word + " is not present in the text.");
+//        }
+
+        //EX7
+//        String text;
+//        String original_word;
+//        String new_word;
+//
+//        System.out.println("Enter the text here: ");
+//        text = scanner.nextLine();
+//
+//        System.out.println("Enter the word need to be replaced: ");
+//        original_word = scanner.nextLine();
+//
+//        System.out.println("Enter the new word: ");
+//        new_word = scanner.nextLine();
+//
+//        System.out.println(text.replaceAll(original_word, new_word));
+
+        //EX8
+//        String input1;
+//        String input2;
+//
+//        System.out.println("Enter the first string");
+//        input1 = scanner.nextLine();
+//
+//        System.out.println("Enter the second string");
+//        input2 = scanner.nextLine();
+//
+//        if (input1.equalsIgnoreCase(input2)){
+//            System.out.println("The strings are equals (ignoring the case sensitivity)");
+//        }
+//        else{
+//            System.out.println("The strings are not equals");
+//        }
+    }
+}
+
