752c1dcfdd168aa0003752dce41c7a5f86c926f6
Omar Alkhuwaytim <84483586+Omaraak@users.noreply.github.com>
2024-07-02 13:10:51 +0300

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -53,20 +53,13 @@ public class Exercise_2 {
         System.out.println("Enter a your grade: ");
         int grade = scanner.nextInt();
 
-        if (grade>=95)
-            System.out.println("A+");
-        else if (grade>=90) {
+       
+        if (grade>=90) {
             System.out.println("A");
-        } else if (grade>=85) {
-            System.out.println("B+");
         } else if (grade>=80) {
             System.out.println("B");
-        }else if (grade>=75) {
-            System.out.println("C+");
         } else if (grade>=70) {
             System.out.println("C");
-        } else if (grade>=65) {
-            System.out.println("D+");
         } else if (grade>=60) {
             System.out.println("D");
         }else
